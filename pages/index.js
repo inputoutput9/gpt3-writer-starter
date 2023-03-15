4f79c1234e5991e27622b4813a7a04751262d74e
inputoutput9 <127095891+inputoutput9@users.noreply.github.com>
2023-03-15 21:02:36 +0000

    Thu, Mar 16, 2023, 6:02 AM +09:00

--- pages/index.js
+++ pages/index.js
@@ -59,9 +59,9 @@ const onUserChangedText = (event) => {
             className="prompt-box"
             placeholder={`For best results, state the problem you solve, solution, and customer.\n
         Protip \➪ Define the problem, solution, and customer with a colon
-        Example \➪ The Problem: It's hard as fu*k to code!
-                                   My Solution: We make it easy as sh*t to code.
-                                   My Customer: People that hate to code but need to.
+        Example input \➪ " The Problem: It's hard as fu*k to code!"
+                                                " My Solution: We make it easy as sh*t to code. "
+                                                " My Customer: People that hate to code but need to. "
                      `}
             value={userInput}
             onChange={onUserChangedText}
