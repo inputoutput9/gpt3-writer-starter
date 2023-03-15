b81ec7395b5a734bdf985f2bf808763fcf0aaa97
inputoutput9 <127095891+inputoutput9@users.noreply.github.com>
2023-03-15 21:56:30 +0000

    Thu, Mar 16, 2023, 6:56 AM +09:00

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ const onUserChangedText = (event) => {
             <h2><br />
                 Step 1: Tell me about your startup idea.<br />
                 Step 2: Instantly get an elevator pitch.<br />
-                Step 3: Go raise investor $$$.<br /><br />
+                Step 3: Go raise investor $$$.
                 {/*<span style={{ fontSize: "10px", textAlign: "right", display: "block" }}>
                   brought to you by:{" "}
                   <a href="https://goka.xyz/" style={{ color: "white" }}>
